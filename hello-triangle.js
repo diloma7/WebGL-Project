5665bc743113b783dcc1fff3d92172cdef273dc9
Diloma Ouattara <d.ouattara@qm-tek.com>
2025-06-14 21:49:47 +0200

    Testing the commit command

--- hello-triangle.js
+++ hello-triangle.js
@@ -1,17 +1,13 @@
 function showError(errorText) {
-
     const erroBoxDiv= document.getElementById('error-box');
     const errorTextElement = document.createElement('p');
     errorTextElement.innerText = errorText;
     erroBoxDiv.appendChild(errorTextElement);
 }
-
 showError('This is what an error looks like!');
-
 function helloTriangle() {
     /**@type {HTMLCanvasElement | null}**/
     const canvas = document.getElementById('demo-canvas');
-
     if (!canvas) {
     showError('Cannot get demo-canvas reference. Check for typos or loading script too early in HTMl');
     return;
